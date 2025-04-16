96857c2aefdc6d2511e5c857b762046ee4218fa9
asyed04 <asyed4@academic.rrc.ca>
2025-04-15 19:44:15 -0500

    Initial commit

---
+++ test-prettier-3.js
@@ -0,0 +1,13 @@
+// This is a third test file with poor formatting
+function thirdTestFunction() {
+  const c = 100;
+  const d = 200;
+
+  return c + d;
+}
+
+const thirdObj = {
+  p: 10,
+  q: 20,
+  r: 30,
+};
