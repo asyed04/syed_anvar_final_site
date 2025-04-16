ba1eb08c060ccf35117213fbdb7080a20ce26370
asyed04 <asyed4@academic.rrc.ca>
2025-04-15 19:31:11 -0500

    Second test for Husky and Prettier

---
+++ test-prettier-2.js
@@ -0,0 +1,13 @@
+// This is another test file with poor formatting
+function anotherTestFunction() {
+  const a = 10;
+  const b = 20;
+
+  return a + b;
+}
+
+const anotherObj = {
+  x: 1,
+  y: 2,
+  z: 3,
+};
